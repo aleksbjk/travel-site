d6fff152d233109bfcd38146d2acf00f83cb853f
Aleks <aleksander.bujko@hotmail.com>
2020-03-15 13:08:13 +0100

    Added sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,10 +1,12 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu.js'
 import RevealOnScroll from './modules/RevealOnScroll.js'
+import StickyHeader from './modules/StickyHeader.js'
 
 let mobileMenu = new MobileMenu();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+let stickyHeader = new StickyHeader();
 
 
 if(module.hot) {
